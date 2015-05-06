9fbac9235e366a3f6c07b6cd1b96a724205ff7d7
Geoff Sobering <geoff@performance-sailing-products.com>
2015-05-06 10:31:44 -0500

    Reduced the space between grid blocks to a constant 4 pixels.

--- src/thumbnail_template.f
+++ src/thumbnail_template.f
@@ -1,7 +1,7 @@
 <fragment>
-<div class="col-xs-6 col-sm-3 col-md-2">
+<div class="col-xs-6 col-sm-3 col-md-2" style="width:134px;padding-left:0px;padding-right:0;margin: 2px 2px 2px 2px;">
     <div class="gridElem">
-        <a href=""><img class="indexGrid" src="" style=""/></a>
+        <a href=""><img class="indexGrid" src="" /></a>
     </div>
 </div>
 </fragment>
